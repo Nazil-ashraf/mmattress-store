9fc2a7848c539c03d8263b174313c0f06a9fe46f
Anshad Mohamed <anshadkm@gmail.com>
2021-09-18 21:36:50 +0530

    added mattresses

--- src/mock/index.js
+++ src/mock/index.js
@@ -12,15 +12,18 @@ export const createMockServer = () => {
         environment: "development",
         models: {
             user: Model.extend(),
-            mattress: Model.extend()
+            mattress: Model.extend(),
+            cart: Model.extend()
         },
         seeds(server) {
             server.createList('user', 200)
             server.createList('mattress', 200)
+            server.createList('cart', 5)
         },
         factories: {
             user: randomUsers,
             mattress: randomMattresses,
+            cart: randomCart
         },
         serializers: {
             application: ApplicationSerializer,
@@ -33,11 +36,17 @@ export const createMockServer = () => {
                 normalize(json) {
                     return normalize(this.schema, 'mattress', json)
                 }
+            }),
+            cart: ApplicationSerializer.extend({
+                normalize(json) {
+                    return normalize(this.schema, 'cart', json)
+                }
             })
         },
         routes() {
             this.resource("users", { path: "/api/users", timing: 500 })
             this.resource("mattresses", {path: "/api/mattresses", timing: 500})
+            this.resource("mattresses", {path: "/api/cart", timing: 500})
         }
     })
 }
@@ -46,12 +55,16 @@ export const randomMattresses = Factory.extend({
     description() { return faker.lorem.paragraph() },
     image() { return faker.image.abstract() },
     size() { return faker.random.arrayElement(["single","double","queen","king"]) },
-    type() { faker.random.arrayElement(["medicated", "ortho"]) },
+    type() { return faker.random.arrayElement(["medicated", "ortho"]) },
     color() { return faker.internet.color() },
     price() { return faker.finance.amount() },
     height() { return faker.datatype.number({'min': 30, 'max': 100}) },
     layers() { return faker.datatype.number({'min': 2, 'max': 5}) },
-    comfort() { faker.random.arrayElement(["form","soft","cotton"]) }
+    comfort() { return faker.random.arrayElement(["form","soft","cotton"]) }
+})
+
+export const randomCart = Factory.extend({
+    //items: []
 })
 
 export const randomUsers = Factory.extend({
