718740d18e2bd8e2fed0949cf40d61d7d6bba133
Anshad Mohamed <anshadkm@gmail.com>
2021-09-18 20:52:59 +0530

    added mattresses

--- src/mock/index.js
+++ src/mock/index.js
@@ -11,13 +11,16 @@ export const createMockServer = () => {
     return createServer({
         environment: "development",
         models: {
-            user: Model.extend()
+            user: Model.extend(),
+            mattress: Model.extend()
         },
         seeds(server) {
             server.createList('user', 200)
+            server.createList('mattress', 200)
         },
         factories: {
-            user: randomUsers
+            user: randomUsers,
+            mattress: randomMattresses,
         },
         serializers: {
             application: ApplicationSerializer,
@@ -25,13 +28,31 @@ export const createMockServer = () => {
                 normalize(json) {
                     return normalize(this.schema, 'user', json)
                 }
+            }),
+            mattress: ApplicationSerializer.extend({
+                normalize(json) {
+                    return normalize(this.schema, 'mattress', json)
+                }
             })
         },
         routes() {
             this.resource("users", { path: "/api/users", timing: 500 })
+            this.resource("mattresses", {path: "/api/mattresses", timing: 500})
         }
     })
 }
+
+export const randomMattresses = Factory.extend({
+    description() { return faker.lorem.paragraph() },
+    image() { return faker.image.abstract() },
+    size() { return faker.random.arrayElement(["single","double","queen","king"]) },
+    type() { faker.random.arrayElement(["medicated", "ortho"]) },
+    color() { return faker.internet.color() },
+    price() { return faker.finance.amount() },
+    height() { return faker.datatype.number({'min': 30, 'max': 100}) },
+    layers() { return faker.datatype.number({'min': 2, 'max': 5}) },
+    comfort() { faker.random.arrayElement(["form","soft","cotton"]) }
+})
 
 export const randomUsers = Factory.extend({
     username() {
