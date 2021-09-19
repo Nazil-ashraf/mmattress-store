389c50802be7bed840b325e88a6d3af8d89bae98
Nimitha <chippy.nimitha@gmail.com>
2021-09-19 20:51:22 +0530

    added login

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,13 @@
 import {Route, Switch} from "react-router-dom";
 import Listing from "./pages/listing/listing";
 import ProductDetails from "./pages/listing/product-details";
+import Login from "./pages/login/login";
 
 const Routes = () => {
     return (
         <div className="routes">
             <Switch>
+                <Route path="/login" component={Login} />
                 <Route path="/mattress" exact component={Listing} />
                 <Route path="/mattress/:id?" component={ProductDetails}/>
             </Switch>
